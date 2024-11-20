9303042e408976771e5068006f5da53ed6553844
[CLOUD4] 홍민영 <47109@naver.com>
2024-11-20 16:28:28 +0900

    initial commit

---
+++ src/main/java/com/team2/mosoo_backend/MosooBackendApplication.java
@@ -0,0 +1,13 @@
+package com.team2.mosoo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MosooBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MosooBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team2/mosoo_backend/MosooBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team2.mosoo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MosooBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
