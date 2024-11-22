6108c4be297af7c2dcfccdb844251a0597788518
[CLOUD4] 홍민영 <47109@naver.com>
2024-11-22 13:54:45 +0900

    feat: #1 CategoryMapper 작성

---
+++ src/main/java/com/team2/mosoo_backend/category/mapper/CategoryMapper.java
@@ -0,0 +1,14 @@
+package com.team2.mosoo_backend.category.mapper;
+
+import com.team2.mosoo_backend.category.dto.CategoryRequestDto;
+import com.team2.mosoo_backend.category.entity.Category;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+public interface CategoryMapper {
+    CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
+
+    @Mapping(source = "parentId", target = "parent.id")
+    @Mapping(target = "level", ignore = true) // level 매핑 제외
+    Category toEntity(CategoryRequestDto request);
+}
