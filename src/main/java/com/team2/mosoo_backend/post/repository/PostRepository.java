7af7e13047a07463c080ca00f93aa0689528d867
[CLOUD4] 박진영 <wlsdud2528@naver.com>
2024-11-25 15:10:05 +0900

    refactor: PostRepository 쿼리 메소드 명 규칙 오류 수정

--- src/main/java/com/team2/mosoo_backend/post/repository/PostRepository.java
+++ src/main/java/com/team2/mosoo_backend/post/repository/PostRepository.java
@@ -4,6 +4,7 @@ package com.team2.mosoo_backend.post.repository;
 import com.team2.mosoo_backend.post.entity.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,7 +13,7 @@ import java.util.List;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    Page<Post> findAllByIsOffer(PageRequest pageRequest, boolean isOffer);
+    Page<Post> findByIsOffer(Pageable pageable, boolean isOffer);
 
-    Page<Post> findAll(PageRequest pageRequest);
+    Page<Post> findById(Pageable pageable, long id);
 }
