faec950297aa66e49d0153dbd06a1cf41dacdf84
[CLOUD4] 김다애 <wlsemrdl1212@naver.com>
2024-11-25 16:15:01 +0900

    feat : Payment entity 작성

--- src/main/java/com/team2/mosoo_backend/payment/entity/Payment.java
+++ src/main/java/com/team2/mosoo_backend/payment/entity/Payment.java
@@ -1,4 +1,52 @@
 package com.team2.mosoo_backend.payment.entity;
 
+import com.team2.mosoo_backend.post.entity.Post;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.joda.time.DateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "payment_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private Long payment_uid;
+
+    @Column(nullable = false)
+    private Long userId;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @Column(nullable = false)
+    private Long amount;
+
+    @Column(nullable = false)
+    private String status;
+
+    private DateTime requested_at;
+
+    private DateTime paid_at;
 }
