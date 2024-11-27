b8d0e8498eced0c35cc9ad519fe3b0892e2ad6c8
[CLOUD4] 차현승 <gustmd9694@gmail.com>
2024-11-27 18:27:07 +0900

    fix: BycryptPasswordEncoder 빈추가 부분 누락 수정, 유저정보 잘못된 필드명 수정

--- src/main/java/com/team2/mosoo_backend/user/service/UserService.java
+++ src/main/java/com/team2/mosoo_backend/user/service/UserService.java
@@ -85,7 +85,7 @@ public class UserService {
         Users user = findById(userDto.getUserId());
 
         //Users 엔티티에 있는 userId 값을 찾아서 반환
-        UsersInfo usersInfo = userInfoRepository.findByUsers_UserId(userInfoId).orElse(null);
+        UsersInfo usersInfo = userInfoRepository.findByUsersId(userInfoId).orElse(null);
         // 회원정보가 이미 있다면 업데이트, 그렇지 않다면 생성
         if (usersInfo != null) {
             usersInfo.updateUserInfo(userUpdateRequestDto);
@@ -97,7 +97,7 @@ public class UserService {
 
     @Transactional
     public void updateGoogleUser(UserDto userDto, UserUpdateRequestDto userUpdateRequestDto, Long userInfoId) {
-        UsersInfo usersInfo = userInfoRepository.findByUsers_UserId(userInfoId).orElse(null);
+        UsersInfo usersInfo = userInfoRepository.findByUsersId(userInfoId).orElse(null);
         Users user = findById(userDto.getUserId());
 
         // 회원정보가 이미 있다면 업데이트 그렇지않다면 생성
