a9344cef9721c3d190d9ecac5397264ef0107c59
[CLOUD4] 안창민 <chm20060@gmail.com>
2024-11-28 17:00:46 +0900

    feat: #31 ChatMessageResponseWrapperDto 수정 - PostResponseDto 사용으로 게시글 정보 포함 - price 필드 유지 (게시글의 가격이 아닌 채팅방의 가격을 포함해야 하므로)

--- src/main/java/com/team2/mosoo_backend/chatting/dto/ChatMessageResponseWrapperDto.java
+++ src/main/java/com/team2/mosoo_backend/chatting/dto/ChatMessageResponseWrapperDto.java
@@ -1,6 +1,7 @@
 package com.team2.mosoo_backend.chatting.dto;
 
 import com.team2.mosoo_backend.bid.dto.BidResponseDto;
+import com.team2.mosoo_backend.post.dto.PostResponseDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,9 +15,9 @@ public class ChatMessageResponseWrapperDto {
 
     private String opponentFullName;    // 채팅 참여하는 상대방 이름
 
-    private Long postId;        // 파생된 게시글 id
-    private String postTitle;   // 파생된 게시글 제목
+    private PostResponseDto postResponseDto; // 파생된 게시글 정보
 
+    // 위의 dto에는 게시글의 가격이 포함되므로 채팅방의 가격 필드 따로 생성
     private int price;
 
     private List<ChatMessageResponseDto> chatMessageResponseDtoList;
