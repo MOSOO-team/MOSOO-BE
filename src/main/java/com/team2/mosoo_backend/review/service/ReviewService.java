23da258f2ff1784fcb50e7d6a10e43ab4ea83e90
A540 <51090700+A540@users.noreply.github.com>
2024-11-28 16:32:58 +0900

    feat: ReviewService 유저 아이디로 리뷰 조회 기능 추가 생성 시 매핑 초기화 추가

--- src/main/java/com/team2/mosoo_backend/review/service/ReviewService.java
+++ src/main/java/com/team2/mosoo_backend/review/service/ReviewService.java
@@ -3,6 +3,8 @@ package com.team2.mosoo_backend.review.service;
 
 import com.team2.mosoo_backend.exception.CustomException;
 import com.team2.mosoo_backend.exception.ErrorCode;
+import com.team2.mosoo_backend.post.entity.Post;
+import com.team2.mosoo_backend.post.repository.PostRepository;
 import com.team2.mosoo_backend.review.dto.CreateReviewRequestDto;
 import com.team2.mosoo_backend.review.dto.ReviewListResponseDto;
 import com.team2.mosoo_backend.review.dto.ReviewResponseDto;
@@ -10,6 +12,8 @@ import com.team2.mosoo_backend.review.dto.UpdateReviewRequestDto;
 import com.team2.mosoo_backend.review.entity.Review;
 import com.team2.mosoo_backend.review.mapper.ReviewMapper;
 import com.team2.mosoo_backend.review.repository.ReviewRepository;
+import com.team2.mosoo_backend.user.entity.Users;
+import com.team2.mosoo_backend.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,7 +26,9 @@ import java.util.List;
 public class ReviewService {
 
     private final ReviewRepository reviewRepository;
+    private final UserRepository userRepository;
     private final ReviewMapper reviewMapper;
+    private final PostRepository postRepository;
 
 
     public ReviewListResponseDto getReviewByPostId(Long postId) {
@@ -39,10 +45,26 @@ public class ReviewService {
 
     }
 
+    public ReviewListResponseDto getReviewByUserId(Long userId) {
+        List<Review> reviewList = reviewRepository.findAllByUserId(userId);
+        List<ReviewResponseDto> reviewResponseDtoList = new ArrayList<>();
+
+
+        for (Review review : reviewList) {
+            reviewResponseDtoList.add(reviewMapper.reviewToReviewResponseDto(review));
+        }
+
+        return new ReviewListResponseDto(reviewResponseDtoList);
+    }
+
     @Transactional
-    public ReviewResponseDto createReview(CreateReviewRequestDto createReviewRequestDto) {
+    public ReviewResponseDto createReview(Long userId, Long postId, CreateReviewRequestDto createReviewRequestDto) {
 
         Review review = reviewMapper.createReviewRequestDtoToReview(createReviewRequestDto);
+        Users user = userRepository.findById(userId).orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+        Post post = postRepository.findById(postId).orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
+
+        review.setMapping(user, post);
 
         Review savedReview = reviewRepository.save(review);
 
@@ -68,4 +90,6 @@ public class ReviewService {
 
         return reviewMapper.reviewToReviewResponseDto(review);
     }
+
+
 }
