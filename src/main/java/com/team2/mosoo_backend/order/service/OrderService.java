8f310f579f2cf6c5da438c07695492679f4aa321
A540 <51090700+A540@users.noreply.github.com>
2024-11-28 15:29:22 +0900

    feat: OrderService 주문 전체 조회 로직 추가

--- src/main/java/com/team2/mosoo_backend/order/service/OrderService.java
+++ src/main/java/com/team2/mosoo_backend/order/service/OrderService.java
@@ -4,12 +4,16 @@ package com.team2.mosoo_backend.order.service;
 import com.team2.mosoo_backend.exception.CustomException;
 import com.team2.mosoo_backend.exception.ErrorCode;
 import com.team2.mosoo_backend.order.dto.CreateOrderRequestDto;
+import com.team2.mosoo_backend.order.dto.OrderListResponseDto;
 import com.team2.mosoo_backend.order.dto.OrderResponseDto;
 import com.team2.mosoo_backend.order.entity.Order;
 import com.team2.mosoo_backend.order.mapper.OrderMapper;
 import com.team2.mosoo_backend.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -18,15 +22,30 @@ public class OrderService {
     private final OrderMapper orderMapper;
 
 
+    public OrderListResponseDto getAllOrders() {
+        List<Order> orders = orderRepository.findAll();
+        List<OrderResponseDto> orderResponseDtoList = new ArrayList<>();
+
+        for(Order order : orders) {
+            orderResponseDtoList.add(orderMapper.orderToOrderResponseDto(order));
+        }
+
+        return new OrderListResponseDto(orderResponseDtoList);
+
+    }
+
     public OrderResponseDto getOrderById(Long orderId) {
         Order order = orderRepository.findById(orderId).orElseThrow(() -> new CustomException(ErrorCode.ORDER_NOT_FOUND));
 
         return orderMapper.orderToOrderResponseDto(order);
     }
 
+
     public OrderResponseDto createOrder(CreateOrderRequestDto createOrderRequestDto) {
         Order order = orderMapper.createOrderRequestDtoToOrder(createOrderRequestDto);
 
         return orderMapper.orderToOrderResponseDto(orderRepository.save(order));
     }
+
+
 }
