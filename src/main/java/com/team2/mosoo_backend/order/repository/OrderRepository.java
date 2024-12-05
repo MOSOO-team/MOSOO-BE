96ceb9d1c15fc3e6f284ef0caf3aac2659b6caa7
[CLOUD4] 김다애 <wlsemrdl1212@naver.com>
2024-12-05 21:10:05 +0900

    feat : #52 OrderRepository findOrdersByChatRoomUserIdAndOrderStatus 작성

--- src/main/java/com/team2/mosoo_backend/order/repository/OrderRepository.java
+++ src/main/java/com/team2/mosoo_backend/order/repository/OrderRepository.java
@@ -2,6 +2,8 @@ package com.team2.mosoo_backend.order.repository;
 
 
 import com.team2.mosoo_backend.order.entity.Order;
+import com.team2.mosoo_backend.order.entity.OrderStatus;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,7 @@ import org.springframework.stereotype.Repository;
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
     Order findByMerchantUid(String merchantUid);
+
+    List<Order> findOrdersByChatRoomUserIdAndOrderStatus(Long userId, OrderStatus status);
+
 }
