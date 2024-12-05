6ee2adbdd220a11ec9d3cf39ac57e1ffe4dea552
[CLOUD4] 김다애 <wlsemrdl1212@naver.com>
2024-12-05 21:12:21 +0900

    feat : #52 createOrderRequestDto 필드 수정

--- src/main/java/com/team2/mosoo_backend/order/dto/CreateOrderRequestDto.java
+++ src/main/java/com/team2/mosoo_backend/order/dto/CreateOrderRequestDto.java
@@ -1,6 +1,5 @@
 package com.team2.mosoo_backend.order.dto;
 
-import com.team2.mosoo_backend.order.entity.Status;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,9 +10,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CreateOrderRequestDto {
 
-    private int price;
 
-    private Status status;
 
-    private String method;
+
+
 }
