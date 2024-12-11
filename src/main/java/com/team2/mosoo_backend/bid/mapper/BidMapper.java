a95e8181860365186492f147ad6076d2d6673b35
[CLOUD4] 안창민 <chm20060@gmail.com>
2024-12-11 11:41:26 +0900

    refactor: #75 bid -> MyBidResponseDto로 변환하는 메서드 추가

--- src/main/java/com/team2/mosoo_backend/bid/mapper/BidMapper.java
+++ src/main/java/com/team2/mosoo_backend/bid/mapper/BidMapper.java
@@ -3,6 +3,7 @@ package com.team2.mosoo_backend.bid.mapper;
 
 import com.team2.mosoo_backend.bid.dto.BidResponseDto;
 import com.team2.mosoo_backend.bid.dto.CreateBidRequestDto;
+import com.team2.mosoo_backend.bid.dto.MyBidResponseDto;
 import com.team2.mosoo_backend.bid.entity.Bid;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -11,6 +12,7 @@ import org.mapstruct.Mapping;
 public interface BidMapper {
 
     BidResponseDto bidToBidResponseDto(Bid bid);
+    MyBidResponseDto bidToMyBidResponseDto(Bid bid);
 
     @Mapping(target = "post", ignore = true)
     Bid createBidRequestDtoToBid(CreateBidRequestDto createBidRequestDto);
